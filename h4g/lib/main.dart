ec5db0f1782d44fd32240bb9d96165079c71228c
ChongWeiJie29 <chongweijie29@gmail.com>
2023-02-11 01:29:29 +0800

    HomePage1

--- h4g/lib/main.dart
+++ h4g/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,96 +10,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: NavBar(),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xff5A3D91),
-                  Color(0xffC8A2C8),
-                ],
-              ),
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
             ),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Tech & Zen',
-                    style: TextStyle(
-                      fontSize: 48.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 16.0),
-                  Text(
-                    'Bringing technology and mindfulness together',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.grey[200],
-                    ),
-                  ),
-                  SizedBox(height: 32.0),
-                  Container(
-                    height: 200.0,
-                    width: 200.0,
-                    decoration: BoxDecoration(
-                      color: Colors.lightBlueAccent,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Icon(
-                      Icons.headset,
-                      size: 100.0,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            title: const Text('Tabs Demo'),
+          ),
+          body: const TabBarView(
+            children: [
+              Icon(Icons.directions_car),
+              Icon(Icons.directions_transit),
+              Icon(Icons.directions_bike),
+            ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class NavBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Text('Tech & Zen'),
-        Row(
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: Text('Home'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Profile'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Collaborators'),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text('Create New'),
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
