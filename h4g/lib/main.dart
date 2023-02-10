7804d5820f9a7cbfcf082f63db8312e290f38e56
ChongWeiJie29 <chongweijie29@gmail.com>
2023-02-11 02:05:05 +0800

    HomePage2

--- h4g/lib/main.dart
+++ h4g/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "HomePage.dart";
 
 void main() {
   runApp(const MyApp());
@@ -9,29 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-            title: const Text('Tabs Demo'),
-          ),
-          body: const TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
-            ],
-          ),
-        ),
-      ),
-    );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
