eb83b9bb85bd6b34ccf7f842415058ecb39ca9c6
Nicholas <nicholastanyunyu@gmail.com>
2023-02-11 05:16:43 +0800

    fixed merging errors

--- h4g/lib/ForumPage.dart
+++ h4g/lib/ForumPage.dart
@@ -1,25 +1,25 @@
-import 'package:flutter/material.dart';
-import 'Button.dart';
+// import 'package:flutter/material.dart';
+// import 'Button.dart';
 
-class ForumPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Forum'),
-      ),
-      body: AddPostForm(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddPostPage(),
-            ),
-          );
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+// class ForumPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Forum'),
+//       ),
+//       body: AddPostForm(),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           Navigator.push(
+//             context,
+//             MaterialPageRoute(
+//               builder: (context) => AddPostPage(),
+//             ),
+//           );
+//         },
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
